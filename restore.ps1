183c96d2260801174b12c7fb125a56c6207fcf61
attolee <lishaotuo@silveruc.com>
2024-02-26 12:16:38 +0800

    fix organization repos missing when restore

--- restore.ps1
+++ restore.ps1
@@ -36,18 +36,19 @@ $PASSWORD = "gitea"
 $DATABASE = "gitea"
 docker exec -u root -it -w /tmp mysql /bin/bash -c "mysql --default-character-set=utf8mb4 -u$USER -p$PASSWORD $DATABASE < gitea-db.sql"
 
+Write-Host '清理临时文件...'
+Remove-Item -Path ./tmp -Recurse -Force
+
 # https://github.com/go-gitea/gitea/issues/23964#issuecomment-1500105529
 Write-Host '恢复 Gitea 仓库中...'
 docker exec -u root -it gitea /bin/bash -c "rm -rf /tmp/*"
 docker cp $BACKUP_FILE gitea:/tmp/gitea-dump.zip
 docker exec -u root -it gitea /bin/bash -c "unzip /tmp/gitea-dump.zip -d /tmp/gitea-dump"
-docker exec -u root -it gitea /bin/bash -c "cp -rf /tmp/gitea-dump/data/* /data/gitea"
+docker exec -u root -it gitea /bin/bash -c "cp -rf /tmp/gitea-dump/data/* /data/gitea/"
+docker exec -u root -it gitea /bin/bash -c "mkdir -p /data/git/repositories"
 docker exec -u root -it gitea /bin/bash -c "cp -rf /tmp/gitea-dump/repos/* /data/git/repositories/"
 docker exec -u root -it gitea /bin/bash -c "chown -R git:git /data"
 docker exec -u git -it gitea /bin/bash -c "/usr/local/bin/gitea -c '/data/gitea/conf/app.ini' admin regenerate hooks"
-
-Write-Host '清理临时文件...'
-Remove-Item -Path ./tmp -Recurse -Force
 
 Write-Host '恢复完成，重启 Gitea...'
 docker compose restart
