8554e38b0dfa6baf308f3d42c7c6b2c7325258de
attolee <lishaotuo@silveruc.com>
2024-02-26 13:42:06 +0800

    update schedule script

--- schedule.ps1
+++ schedule.ps1
@@ -1,21 +1,16 @@
-# 定义任务名，脚本路径和脚本文件名
-$taskName = "BackupTask"
+param(
+    [string]$BACKUP_DIR
+)
+
+$taskName = "Gitea Backup"
 $scriptPath = (Get-Location).Path
 $scriptName = "backup.ps1"
 
-# 创建触发器
 $trigger = New-ScheduledTaskTrigger -Daily -At 4am
-
-# 创建操作
-$action = New-ScheduledTaskAction -Execute "Powershell.exe" -Argument "-File $scriptPath\$scriptName"
-
-# 创建主体
+$action = New-ScheduledTaskAction -Execute "Powershell.exe" -Argument "-File `"$scriptPath\$scriptName`" $BACKUP_DIR"
 $principal = New-ScheduledTaskPrincipal -UserId "$env:USERDOMAIN\$env:USERNAME" -LogonType Interactive
-
-# 创建任务
 $task = New-ScheduledTask -Action $action -Principal $principal -Trigger $trigger
 
-# 注册任务
 if (Get-ScheduledTask -TaskName $taskName -ErrorAction SilentlyContinue) {
     Unregister-ScheduledTask -TaskName $taskName -Confirm:$false
 }
