16e0bee197aa5ba3122b305b15fa0fb88064a184
李绍托 <attolee@outlook.com>
2024-03-05 23:44:16 +0800

    fix the input device is not a TTY when backup by scheduler

--- backup.sh
+++ backup.sh
@@ -12,8 +12,8 @@ then
     mkdir -p $BACKUP_DIR
 fi
 
-docker exec -u root -it -w /tmp gitea /bin/bash -c 'rm -rf *'
-docker exec -u git -it -w /tmp gitea /bin/bash -c '/usr/local/bin/gitea dump -c /data/gitea/conf/app.ini'
+docker exec -u root -w /tmp gitea /bin/bash -c 'rm -rf *'
+docker exec -u git -w /tmp gitea /bin/bash -c '/usr/local/bin/gitea dump -c /data/gitea/conf/app.ini'
 docker cp gitea:/tmp/ .
 mv ./tmp/*.zip $BACKUP_DIR
 rm -rf ./tmp
